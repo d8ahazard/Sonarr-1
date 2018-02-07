cacaef2d0d2096aeeaf5882b56282b22335b0bc1
Mark McDowall <mark@mcdowall.ca>
2018-02-06 17:58:15 -0800

    fixup! Updated Debian package

--- distribution/debian.sh
+++ distribution/debian.sh
@@ -4,7 +4,7 @@ echo Version: "$dependent_build_number" Branch: "$dependent_build_branch"
 rm -r ./sonarr_bin/Sonarr.Update
 
 echo Updating changelog
-sed -i "s/{version}/($dependent_build_number)/g" debian/changelog
+sed -i "s/{version}/$dependent_build_number/g" debian/changelog
 sed -i "s/{branch}/$dependent_build_branch/g" debian/changelog
 
 echo Running debuild
